e2013264fa5881fbf9851082015437bccf2c13e0
CristoLoya <30960998+CristoLoya@users.noreply.github.com>
2017-08-17 23:09:27 -0500

    Add files via upload

---
+++ fun_with_numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  unsigned long int first_value, second_value, sum, difference, product, integer_based_division, remainder_of_integer_division;
+
+  cout << "enter first value" << endl;
+  cin >> first_value;
+  cout << "enter second value" << endl;
+  cin >> second_value;
+
+  sum = first_value + second_value;
+  difference = first_value - second_value;
+  product = first_value * second_value;
+  integer_based_division = first_value / second_value;
+  remainder_of_integer_division = first_value % second_value;
+
+  cout << "sum = " << sum << endl;
+  cout << "difference = " << difference << endl;
+  cout << "product = " << product << endl;
+  cout << "integer based division = " << integer_based_division << endl;
+  cout << "remainder of integer division = " << remainder_of_integer_division << endl;
+
+  return 0;
+}
