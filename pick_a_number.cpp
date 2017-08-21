e2fb775a25698c39bcfca0f8e8ec7c047aacc919
CristoLoya <30960998+CristoLoya@users.noreply.github.com>
2017-08-20 23:17:07 -0500

    Add files via upload

---
+++ pick_a_number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+  int random, counter, guess;
+
+  srand (time(0));
+
+  random = 1 + (rand() % 100);
+
+  cout << "guess a number between 1 and 100" << endl;
+
+  while (random != guess) {
+    cin >> guess;
+    counter = counter + 1;
+      if (guess < random) {
+        cout << "too low, try again" << endl;
+      }
+      if (guess > random) {
+        cout << "too high, try again" << endl;
+      }
+  }
+  if (counter <= 1){
+    cout << "Congratulations! It only took you 1 attempt" << endl;
+  }
+  else {
+    cout << "Congratulations! It only took you " << counter << " attempts" << endl;
+  }
+return 0;
+}
